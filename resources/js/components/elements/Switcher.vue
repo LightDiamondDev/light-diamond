6d5a38301b3b6828b3279dbf86b30983cc5dc43e
S3v3Nice <s3v3nicee@gmail.com>
2024-07-24 01:17:32 +0300

    Fix formatting in Switcher.vue

--- resources/js/components/elements/Switcher.vue
+++ resources/js/components/elements/Switcher.vue
@@ -1,37 +1,44 @@
 <script setup lang="ts">
-    import {computed} from 'vue'
-    const props = defineProps({
-        active: Boolean|undefined
-    });
-    const model = defineModel<boolean>({default: false})
-    const active = computed(() => props.active ?? model.value)
-    function onClick() {
-        if (props.active === undefined) {}
+import {computed} from 'vue'
+
+const props = defineProps({
+    active: Boolean | undefined
+})
+
+const model = defineModel<boolean>({default: false})
+const active = computed(() => props.active ?? model.value)
+
+function onClick() {
+    if (props.active === undefined) {
         model.value = !model.value
     }
+}
 </script>
 
 <template>
-    <button :class="iconSwitcherWay"
-          class="settings-switcher icon-switcher-way icon flex justify-center items-center"
-          @click="onClick"
+    <button
+        class="settings-switcher icon-switcher-way icon flex justify-center items-center"
+        @click="onClick"
     >
-        <span :class="{ iconSwitcherHandle, 'on': active }"
-            class="handle icon-switcher-handle icon flex justify-center items-center"
-    />
+        <span :class="{ 'on': active }"
+              class="handle icon-switcher-handle icon flex justify-center items-center"
+        />
     </button>
-    </template>
+</template>
 
-    <style scoped>
-    .settings-switcher .handle {
-        background-size: 100% 100%;
-        transform: translateX(40%);
-        position: relative;
-        transition: .5s;
-        height: 32px;
-        width: 32px;
-        margin: 0;
-    }
-    .settings-switcher .handle.on { transform: translateX(-40%); }
+<style scoped>
+.settings-switcher .handle {
+    background-size: 100% 100%;
+    transform: translateX(40%);
+    position: relative;
+    transition: .5s;
+    height: 32px;
+    width: 32px;
+    margin: 0;
+}
+
+.settings-switcher .handle.on {
+    transform: translateX(-40%);
+}
 
 </style>
