c2d25fb79c9137ea563b760a472438955a9b6cc1
GrostLight <ilyagrost584@gmail.com>
2025-01-24 22:10:25 +0300

    LD-44: Updated Favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="icon" type="image/png" href="/images/elements/light-diamond-favicon.png"/>
+        <link rel="icon" type="image/png" href="/public/images/elements/light-diamond-favicon.png"/>
 
         <title>{{ config('app.name') }}</title>
 
